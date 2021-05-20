cd202dc0c96f80d9b49ab2bfc1338205309f7330
federico <federicotelo@gmail.com>
2021-05-20 18:07:05 -0300

    agregamos funcion compartir2

--- js/app.js
+++ js/app.js
@@ -22,7 +22,7 @@ let puso = document.querySelector("input[name=puso]")
 let btn = document.getElementById("btn1")
 let btn2 = document.getElementById("btn2")
 let btn3 = document.getElementById("btn3")
-let btn4 = document.getElementById("share")
+let btn4 = document.getElementById("sharer")
 let p = document.getElementById("prfo")
 let final = document.getElementById("final")
 
